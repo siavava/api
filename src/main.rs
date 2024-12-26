cd8300e75436120840dff8ca334360f1b3fcf6ff
siavava <amittaijoel@outlook.com>
2024-12-25 19:22:02 -0800

    (update): added docker/make file

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,27 @@
 use mongodb::{
-  bson::doc,
   error::Result,
   options::{ClientOptions, ServerApi, ServerApiVersion},
   Client,
 };
 
+use dotenv::dotenv;
+
 use wsserver::views;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  println!("Hello, world!");
-  // TODO: do something here.
+  dotenv().ok();
+  let mongodb_uri = std::env::var("MONGODB_URI").expect("MONGODB_URI not set in .env file");
 
-  let mut client_options =
-    ClientOptions::parse("mongodb+srv://siavava:what2three@blog-feed.vleka.mongodb.net/?retryWrites=true&w=majority&appName=blog-feed").await?;
+  let mut client_options = ClientOptions::parse(mongodb_uri).await?;
 
-  // Set the server_api field of the client_options object to set the version of the Stable API on the client
   let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
   client_options.server_api = Some(server_api);
-  // Get a handle to the cluster
+
   let client = Client::with_options(client_options)?;
-  // Ping the server to see if you can connect to the cluster
-  client
-    .database("admin")
-    .run_command(doc! {"ping": 1})
-    .await?;
-
-  println!("Pinged your deployment. You successfully connected to MongoDB! What else?");
-
   let db = client.database("blog-feed");
 
+  // test connection
   let views = views![&db, "/afternoon"];
 
   println!("views: {:?}", views);
