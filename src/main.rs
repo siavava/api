9a26a98b87bed2f4ff01461e6097e5ca737e1003
siavava <amittaijoel@outlook.com>
2025-04-24 02:21:08 -0700

    (update): fix cors finally??

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use actix_web::{
   App, HttpServer, Responder,
   dev::RequestHead,
   get,
-  http::header::HeaderValue,
+  http::{self, header::HeaderValue},
   middleware::{Logger, NormalizePath, TrailingSlash},
   web,
 };
@@ -69,9 +69,12 @@ async fn main() -> Result<()> {
       .wrap(NormalizePath::new(TrailingSlash::Always))
       .wrap(
         Cors::default()
-          // .allowed_origin("http://localhost:3000")
-          .allowed_origin_fn(verify_cors)
+          .allowed_origin("https://amittai.space")
+          .allowed_origin_fn(|origin, _req_head| origin.as_bytes().ends_with(b"amittai.space"))
+          // .allowed_origin_fn(verify_cors)
           .allowed_methods(vec!["GET", "PUT", "POST", "DELETE"])
+          .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+          .allowed_header(http::header::CONTENT_TYPE)
           .max_age(3600),
       )
       .app_data(web::Data::<AppState>::new(app_state.clone()))
